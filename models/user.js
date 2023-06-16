e81e5f1b0b563b758a8ebba20dc449e4679cc257
alllex123456 <alextanase454@gmail.com>
2023-06-16 15:55:30 +0300

    ---

--- models/user.js
+++ models/user.js
@@ -44,6 +44,7 @@ const userSchema = new Schema(
     ],
     notes: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Note' }],
     bankAccounts: { type: Array, required: false },
+    emailAlerts: { type: Object, required: false },
   },
   { timestamps: true }
 );
